531c3a22f5b6b604f3aea58e2b5f24e5c8b964cb
Peter Störmer <mail@peter-stoermer.de>
2024-09-29 01:09:02 +0200

    chore(task store): add update task function

--- app/utils/store/useTaskStore.ts
+++ app/utils/store/useTaskStore.ts
@@ -6,13 +6,20 @@ type TaskStore = {
   tasks: Task[]
   visibleTaskDetails: { [taskId: string]: boolean }
   setTasks: (tasks: Task[]) => void
+  updateTask: (updatedTask: Task) => void
   toggleTaskDetails: (taskId: string) => void
 }
 
 export const useTaskStore = create<TaskStore>((set) => ({
   tasks: [],
   visibleTaskDetails: {},
+
   setTasks: (tasks) => set({ tasks }),
+
+  updateTask: (updatedTask) =>
+    set((state) => ({
+      tasks: state.tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)),
+    })),
 
   toggleTaskDetails: (taskId) =>
     set((state) => ({
